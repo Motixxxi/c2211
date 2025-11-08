39fce9132128eab2cbf4cb1aedd2ceff8f005547
Motixxxi <kovalenko.matvey18@gmail.com>
2025-11-08 10:10:57 +0200

    Update Library.py

--- Library.py
+++ Library.py
@@ -1,13 +1,1 @@
-from colorama import init, Fore, Back, Style
 
-
-init()
-
-
-print(Fore.RED + "Це червоний текст")
-print(Back.GREEN + "Зелений фон")
-print(Style.BRIGHT + "Яскравий текст")
-print(Style.RESET_ALL + "Повернення до звичайного стилю")
-
-
-print(Fore.YELLOW + Back.BLUE + "Жовтий текст на синьому фоні")
